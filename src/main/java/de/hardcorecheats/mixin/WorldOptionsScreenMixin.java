3f48fc51f37c9f4c62c4f4dfdddb1af2da6d1610
FuchsO7 <mc.fuchs07@gmail.com>
2026-09-21 19:07:30 +0200

    Update Mod to 26.3 Bumped Mod Version to 1.1.0

--- src/main/java/de/hardcorecheats/mixin/WorldOptionsScreenMixin.java
+++ src/main/java/de/hardcorecheats/mixin/WorldOptionsScreenMixin.java
@@ -1,6 +1,6 @@
 package de.hardcorecheats.mixin;
 
-import net.minecraft.client.gui.screens.options.WorldOptionsScreen;
+import net.minecraft.client.gui.screens.WorldOptionsScreen;
 import net.minecraft.client.server.IntegratedServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,7 +13,7 @@ public class WorldOptionsScreenMixin {
             at = @At(value = "INVOKE", target = "Lnet/minecraft/client/server/IntegratedServer;isHardcore()Z"),
             method = "createAllowCommandsButton"
     )
-    private static boolean enableCommandsButtonInHardcore(IntegratedServer instance) {
+    private static boolean hardcorecheats$enableCommandsButtonInHardcore(IntegratedServer instance) {
         return false;
     }
 
@@ -21,7 +21,7 @@ public class WorldOptionsScreenMixin {
             at = @At(value = "INVOKE", target = "Lnet/minecraft/client/server/IntegratedServer;isHardcore()Z"),
             method = "updateButton"
     )
-    private static boolean enableGameRuleAndGameModeButtonInHardcore(IntegratedServer instance) {
+    private static boolean hardcorecheats$enableGameRuleAndGameModeButtonInHardcore(IntegratedServer instance) {
         return false;
     }
 }
